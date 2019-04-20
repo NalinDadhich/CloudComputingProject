5acdb46f333f84070cc4b956693f666a4fce0593
Nalin Dadhich <dadhich.nalin@tamu.edu>
2019-04-20 16:08:17 -0500

    adding server initial class

---
+++ server_create.py
@@ -0,0 +1,20 @@
+from threading import Thread
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+class Wrapper:
+    def __init__(self):
+        print("Hello")
+
+class Server:
+
+    def __init__(self):
+        self.thread = None
+        self.server = None
+
+    def start(self):
+        self.thread = Thread(target=self.server.serveforever)
+        self.server = SimpleWebSocketServer('localhost', 1997, Wrapper)
+        self.thread.start()
+
+    def all_connected(self):
+        return self.server.connections.values()
