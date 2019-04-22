d752d44f3d762304c3539fbba1c1483aeea134c6
Nalin Dadhich <dadhich.nalin@tamu.edu>
2019-04-22 15:57:59 -0500

    main wrapper to all py files

---
+++ main.py
@@ -0,0 +1,21 @@
+from Producer import Producer
+from server_create import Server
+from Consumer import Consumer
+
+producer = Producer(10, 'Movie', 'AIzaSyArJ61vA64yXkZIQeUvl6giqUhVtbPCVAI')
+producer.start()
+print("Producer has started!")
+
+server = Server()
+server.start()
+print("Server started")
+
+
+def broadcast_msg(message):
+    for conn in server.all_connected():
+        conn.sendMessage(message)
+
+
+consumer = Consumer(broadcast_msg)
+consumer.start()
+print("Consumer started")
