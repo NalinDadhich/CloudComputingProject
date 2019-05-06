8159958a983ee19ae956a12b9f348fbd664671dc
Nalin Dadhich <dadhich.nalin@tamu.edu>
2019-05-05 22:08:29 -0500

    server changes modification

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ producer = Producer(10, 'Movie trailers',
 producer.start()
 print("Producer has started!")
 
-# server = Server()
-# server.start()
-# print("Server started")
+server = Server()
+server.start()
+print("Server started")
 
 
-# def broadcast_msg(message):
-#     for conn in server.all_connected():
-#         conn.sendMessage(message)
-#
-#
-# consumer = Consumer(broadcast_msg)
-# consumer.start()
-# print("Consumer started")
+def broadcast_msg(message):
+    for conn in server.all_connected():
+        conn.sendMessage(message)
+
+
+consumer = Consumer(broadcast_msg)
+consumer.start()
+print("Consumer started")
